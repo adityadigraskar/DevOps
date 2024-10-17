67af29cf34ea1ec271c7b0dc0224db2cc07932ff
adityadigraskar <36623860+adityadigraskar@users.noreply.github.com>
2024-10-17 14:27:18 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+ami = "ami-0e8d228ad90af673b" Update with your desired AMI ID
+instance_type = "t2.micro"
+
+tags = {
+Name = "ExampleInstance"
+}
+}
